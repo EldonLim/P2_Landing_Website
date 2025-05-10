257959152d4501204a43a5c67e7992c94a088477
Eldon Lim <eldonlim@Eldons-MacBook-Pro.local>
2025-05-10 23:13:43 +0800

    Made HeroSection responsive

--- landing-website/src/components/HeroSection.tsx
+++ landing-website/src/components/HeroSection.tsx
@@ -12,14 +12,14 @@ interface HeroSectionProps {
 
 const HeroSection: React.FC<HeroSectionProps> = ({ title, subtitle, ctaText1, ctaText2, onCtaClick1, onCtaClick2 }) => {
     return (
-        <div className="py-20 lg:py-50 lg:pl-80 pl-20 pr-20">
+        <div className="py-20 lg:py-50 xl:pl-80 lg:pl-30 pl-10 pr-10 md:pl-20 md:pl-20">
             <motion.div
                 initial={{opacity: 0, y: -90}}
                 animate={{opacity: 1, y: 0}}
                 transition={{duration:0.6, ease: 'easeOut'}}
             >
-                <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold mb-6 lg:mr-150 sm:mr-50">{title}</h1>
-                <p className='lg:text-2xl text-sm text-gray-300 mr-0 lg:mr-200 mb-10'>{subtitle}</p>
+                <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-6 md:mr-50 lg:mr-40 xl:mr-100 sm:mr-50">{title}</h1>
+                <p className='lg:text-2xl md:text-xl text-sm text-gray-300 mr-0 md:mr-50 xl:mr-200 lg:mr-100 mb-10'>{subtitle}</p>
                 <button
                     onClick={onCtaClick1}
                     className='bg-white text-black hover:bg-gray-300 text-md rounded-xl mr-6 px-4 py-2'
